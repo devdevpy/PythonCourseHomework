d5123182d8fb42a750ceaa41f3b10ed055116958
DevPy <devdevpy@gmail.com>
2023-08-28 21:43:59 +0300

    Homework from Lecture4!

---
+++ Lecture4/Problem12.py
@@ -0,0 +1,13 @@
+age = int(input("Write your years: "))
+gender = input("Write your gender ('M' for Male 'F' for Female): ").upper()
+marital_status = input("marital status ('Y' for Yes 'N' for No): ").upper()
+
+if gender == "F":
+    print("You can only work in urban areas.")
+elif gender == "M":
+    if 40 >= age >= 20:
+        print("You can work anywhere.")
+    elif 60 >= age >= 40:
+        print("You can only work in urban areas.")
+    else:
+        print("Error")
