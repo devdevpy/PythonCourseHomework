ae8a7fb042851561aaab5485e0a47b1f430ac281
devdevpy <devdevpy@gmail.com>
2023-09-14 22:09:45 +0300

    Homework from Lecture5!

--- Lecture5/Problem9.py
+++ Lecture5/Problem9.py
@@ -1,7 +1,7 @@
 x1, y1 = int(input()), int(input())
 x2, y2 = int(input()), int(input())
 
-if((x1 < x2) and (y1 < y2)) or ((y1 < x2) and (x1 < y2)):
+if ((x1 < x2) and (y1 < y2)) or ((y1 < x2) and (x1 < y2)):
     print("Вмества се")
 else:
     print("Не се вмества")
