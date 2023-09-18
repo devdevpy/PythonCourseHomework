87288697b917a7035eed532b1bb159906a163d8c
devdevpy <devdevpy@gmail.com>
2023-09-18 19:38:14 +0300

    Homework from Lecture6!

---
+++ Lecture6/Problem3.py
@@ -0,0 +1,13 @@
+count = 0
+
+for a in range(1, 6):
+    count += a
+    stars = "*  " * count
+    print(stars)
+    count = 0
+
+for a1 in range(4, 0, -1):
+    count += a1
+    stars = "*  " * count
+    print(stars)
+    count = 0
