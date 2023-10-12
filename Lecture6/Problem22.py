229eaf3a163cc2af5c6c5db2c7d2576b9ca0d884
devdevpy <devdevpy@gmail.com>
2023-10-12 22:34:12 +0300

    Homework from Lecture6!

--- Lecture6/Problem22.py
+++ Lecture6/Problem22.py
@@ -7,13 +7,11 @@ for i in range(1, 36):
         print(" ")
     elif 6 <= i <= 30:
         count += 1
-        if count % 10 == 1:
+        if count % 10 == 1 or count % 10 == 6:
             print("*", end="   ")
-        if count % 10 == 6:
-            print("*", end="   ")
-        if count % 10 == 5 or count % 10 == 0:
+        elif count % 10 == 5 or count % 10 == 0:
             print("*")
-        if (count % 10 == 2 or
+        elif (count % 10 == 2 or
                 count % 10 == 3 or
                 count % 10 == 4 or
                 count % 10 == 7 or
