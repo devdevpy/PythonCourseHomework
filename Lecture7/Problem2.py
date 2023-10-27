ed4a6abd94909a962844271d9961ff27de8f1453
devdevpy <devdevpy@gmail.com>
2023-10-27 21:41:59 +0300

    Homework from Lecture7!

---
+++ Lecture7/Problem2.py
@@ -0,0 +1,22 @@
+list_ = []
+count = 0
+
+try:
+    while True:
+        num = int(input())
+        if num == 0:
+            break
+        list_.append(num)
+        count += 1
+    if count < 4:
+        print("You must enter a minimum of four digits.")
+        exit()
+except ValueError:
+    print(f"invalid literal for int():")
+
+copy_list = sorted(list_[:])
+
+for i in range(0, 2):
+    copy_list.remove(min(copy_list))
+    copy_list.remove(max(copy_list))
+print(list_, copy_list)
