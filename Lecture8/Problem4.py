67d68be1eb847932012ff90583502254818eeddf
devdevpy <devdevpy@gmail.com>
2023-11-19 22:26:35 +0200

    Homework from Lecture8!

---
+++ Lecture8/Problem4.py
@@ -0,0 +1,20 @@
+city = input()
+dict_ = {}
+
+try:
+
+    while city != "":
+        lots_of_rain = int(input())
+
+        if city in dict_:
+            dict_[city] = lots_of_rain + dict_[city]
+        else:
+            dict_[city] = lots_of_rain
+
+        city = input()
+
+except ValueError:
+    print("invalid literal")
+
+for key, value in dict_.items():
+    print("{}: {}".format(key, value))
